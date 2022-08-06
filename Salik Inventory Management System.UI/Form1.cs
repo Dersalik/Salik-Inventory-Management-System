8c19a13d79e851cee4bc5fc89786e8275d97a934
DerSalik <mahamadarbil44@gmail.com>
2022-08-06 13:47:14 +0300

    Add project files.

---
+++ Salik Inventory Management System.UI/Form1.cs
@@ -0,0 +1,10 @@
+namespace Salik_Inventory_Management_System.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
